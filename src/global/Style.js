ac8455bfaf7831add90c87a53f2f164f140aa2ee
Talhakhilji23 <03136400456oppo@gmail.com>
2022-12-08 19:36:48 +0500

    pushed

---
+++ src/global/Style.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Style() {
+  return (
+    <View>
+      <Text>Style</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
